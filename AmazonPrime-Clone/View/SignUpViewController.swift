cadd290aa2aadc2db2c46f632a1166e5c88d87f5
rohit-56 <rohitmou25@gmail.com>
2023-01-22 13:07:14 +0530

    created login & signup storyboard

--- AmazonPrime-Clone/View/SignUpViewController.swift
+++ AmazonPrime-Clone/View/SignUpViewController.swift
@@ -13,6 +13,8 @@ class SignUpViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
     
 
