f582646ef2267c30834306436d30f9ab3de0254e
rohit-56 <rohitmou25@gmail.com>
2023-01-22 12:52:11 +0530

    created login & signup VC

---
+++ AmazonPrime-Clone/View/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  AmazonPrime-Clone
+//
+//  Created by Rohit Sharma on 22/01/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ AmazonPrime-Clone/View/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  AmazonPrime-Clone
+//
+//  Created by Rohit Sharma on 22/01/23.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
