6f782ee4d91daaea9d0616a920ab76cae75df625
rohit-56 <rohitmou25@gmail.com>
2023-01-27 18:38:00 +0530

    created Home,Downloads,Search and Store VC

---
+++ AmazonPrime-Clone/View/components/DownloadsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadsViewController.swift
+//  AmazonPrime-Clone
+//
+//  Created by Rohit Sharma on 27/01/23.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
---
+++ AmazonPrime-Clone/View/components/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  AmazonPrime-Clone
+//
+//  Created by Rohit Sharma on 27/01/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+
+}
