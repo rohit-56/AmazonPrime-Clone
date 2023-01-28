30a0345c225905dc1cc88035f50095f5cbca2468
rohit-56 <rohitmou25@gmail.com>
2023-01-28 18:34:17 +0530

    created custom tableview cell class

---
+++ AmazonPrime-Clone/View/general/TitleTableViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  TitleTableViewCell.swift
+//  AmazonPrime-Clone
+//
+//  Created by Rohit Sharma on 27/01/23.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+   static let identifier = "TitleTableViewCell"
+    
+    private var collectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 200)
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
+    
+}
+
+extension TitleTableViewCell : UICollectionViewDelegate , UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    
+    
+    
+}
