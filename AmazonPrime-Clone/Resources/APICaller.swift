6e9a1410c80e6dc53b33d96eb1cdeb765cf76403
rohit-56 <rohitmou25@gmail.com>
2023-01-29 12:31:41 +0530

    create APICaller

---
+++ AmazonPrime-Clone/Resources/APICaller.swift
@@ -0,0 +1,17 @@
+//
+//  APICaller.swift
+//  AmazonPrime-Clone
+//
+//  Created by Rohit Sharma on 29/01/23.
+//
+
+import Foundation
+
+
+class APICaller {
+    
+    static let shared = APICaller()
+    
+    
+    
+}
