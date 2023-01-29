0e79ab843b5e53a58f0b8b194c277b623799317a
rohit-56 <rohitmou25@gmail.com>
2023-01-29 13:23:28 +0530

    restore all files

--- AmazonPrime-Clone/View/components/SearchViewController.swift
+++ AmazonPrime-Clone/View/components/SearchViewController.swift
@@ -14,6 +14,7 @@ class SearchViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         navigationItem.titleView =  NavigationView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 40))
+        view.backgroundColor = .systemBackground
     }
     
 
