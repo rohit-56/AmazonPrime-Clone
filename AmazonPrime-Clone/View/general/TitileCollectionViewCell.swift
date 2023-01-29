35c8dbbeb5fa745a63539bf8d8b58e249324a7a5
rohit-56 <rohitmou25@gmail.com>
2023-01-29 12:32:16 +0530

    implement collection view cell

--- AmazonPrime-Clone/View/general/TitileCollectionViewCell.swift
+++ AmazonPrime-Clone/View/general/TitileCollectionViewCell.swift
@@ -9,4 +9,27 @@ import UIKit
 
 class TitileCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = "TitileCollectionViewCell"
+    
+    private var imageView : UIImageView = {
+       var imageView = UIImageView()
+        imageView.image = UIImage(named: "poster")
+        return imageView
+    }()
+  
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = bounds
+    }
+    
 }
